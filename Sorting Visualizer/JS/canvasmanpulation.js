0bc3e87bb868344f4456060748bfc9af4ef0b7ae
OMKAR SINGH <40597416+omkar02032000@users.noreply.github.com>
2021-06-11 00:31:27 +0530

    Delete canvasmanpulation.js

--- Sorting Visualizer/JS/canvasmanpulation.js
+++
@@ -1,9 +0,0 @@
-var canvas = document.querySelector('canvas');
-var c = canvas.getContext("2d");
-canvas.width = 1355//window.innerWidth
-canvas.height = 530//window.innerHeight
-
-
-function clearCanvas(){
-	c.clearRect(0, 0, canvas.width, canvas.height);
-}
