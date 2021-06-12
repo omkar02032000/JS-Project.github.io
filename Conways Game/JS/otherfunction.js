d327d6013c6cd9273a9e4441c6c50c60435810f8
OMKAR SINGH <40597416+omkar02032000@users.noreply.github.com>
2021-06-12 16:38:06 +0530

    Delete otherfunction.js

--- Conways Game/JS/otherfunction.js
+++
@@ -1,9 +0,0 @@
-function drawCheckBoard(arr){
-
-	for(let i=0; i < (ROW_MAX_VALUE * COL_MAX_VALUE); i++){
-		c.beginPath();
-		c.strokeStyle = 'blue';
-		c.rect(arr[i].x, arr[i].y, arr[i].width, arr[i].height);
-		c.stroke();
-	}
-}
